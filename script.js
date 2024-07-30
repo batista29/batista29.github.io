0d2905dce5f40b7ec27b7c89a746e09ee79c2e5a
batista29 <natabatista2908@gmail.com>
2024-07-29 21:20:25 -0300

    Criando funções

---
+++ script.js
@@ -0,0 +1,36 @@
+function projetos() {
+    const project = {
+        1: {
+            'nome': 'PlayOnMatch',
+            'img': './assets/playonmatch.png'
+        },
+        2: {
+            'nome': 'MyProject',
+            'img': 'assets/myy_projects.png'
+        },
+        3: {
+            'nome': 'QuizUp',
+            'img': './assets/quizz.png'
+        },
+        4: {
+            'nome': 'Projeto Integrador 1',
+            'img': 'assets/logo_projeto.png'
+        },
+        5: {
+            'nome': 'Folha de Pagamento',
+            'img': 'assets/folhadepagamento.png'
+        },
+        6: {
+            'nome': 'AgroTech',
+            'img': './assets/agrotech.jpeg'
+        },
+        7: {
+            'nome': 'TechMan',
+            'img': './assets/techman.png'
+        },
+    }
+
+    console.log(project)
+}
+
+console.log(projetos)
